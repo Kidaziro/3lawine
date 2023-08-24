1cf897646a62936ce7aea875429088a3b3c3bc26
Kidaziro <97578887+Kidaziro@users.noreply.github.com>
2023-08-24 17:15:04 +0100

    update with icon

--- script.js
+++ script.js
@@ -215,13 +215,10 @@ background-color: #121713;
 
 `;
 
-const styleElement = document.createElement('style');
+const styleElement = document.createElement("style");
 styleElement.textContent = cssCode;
 
 document.head.appendChild(styleElement);
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   var preloader = document.createElement("div");
@@ -229,8 +226,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Create the image element
   var image = document.createElement("img");
-  image.src = "https://github.com/Kidaziro/3lawine/blob/main/image_processing20200803-12829-111f0pe.gif?raw=true"; // Replace with the path to your imag
-  preloader.appendChild(image); 
+  image.src =
+    "https://github.com/Kidaziro/3lawine/blob/main/image_processing20200803-12829-111f0pe.gif?raw=true"; // Replace with the path to your imag
+  preloader.appendChild(image);
 
   document.body.appendChild(preloader);
 
@@ -240,7 +238,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 2000);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Create the parent container element
   var infoContainer = document.createElement("div");
   infoContainer.classList.add("info");
@@ -255,6 +253,7 @@ document.addEventListener("DOMContentLoaded", function() {
   infoTitle.textContent = "معلومات عنا";
 
   // Create the info-p element
+
   var infoP = document.createElement("p");
   infoP.classList.add("info-p");
   infoP.innerHTML = `عملائنا الكرام،<br>
@@ -279,7 +278,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var targetElement = document.querySelector(".heading-primary");
 
   // Check if the target element exists
-  if (targetElement && targetElement.textContent.includes('عروض مميزة')) {
+  if (targetElement && targetElement.textContent.includes("عروض مميزة")) {
     // Insert the infoContainer element before the target element
     targetElement.parentNode.insertBefore(infoContainer, targetElement);
   }
@@ -290,13 +289,9 @@ document.addEventListener("DOMContentLoaded", function () {
     ".list-unstyled>li>a ,.header-list>li>a,.yc ,.yc-search,.cart-switcher>.currency-value>span"
   );
   for (let i = 0; i < elements.length; i++) {
-    
     elements[i].style.color = "white";
- 
   }
 });
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const elements = document.querySelectorAll(".main-bar,.desktop-bar,footer");
@@ -311,3 +306,243 @@ document.addEventListener("DOMContentLoaded", function () {
     element.style.backgroundColor = "#FFFCF5";
   });
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  const navElement = document.querySelector('nav');
+  const originalLi = navElement.querySelector('li a[href="/collections"]');
+  const newLi = document.createElement('li');
+  const newLocal = (newLi.innerHTML = `
+  <style>
+  
+li a {text-decoration : none; color : #2d2f31;} 
+
+
+.span,.span-02,.span-03,.span-04,.span-05,.span-06 {
+  text-align: right;
+  padding: 15px 0;
+  background : none; 
+  color : white;
+  /* font-size : 1.2em; 
+  font-variant : small-caps; */
+  cursor : pointer;
+  display: block;
+}
+
+.span::after,.span-02::after,.span-03::after,.span-04::after,.span-05::after,.span-06::after {
+  float: left;
+  color: #747474;
+  content: "▾";
+}
+
+.slide,.slide-02,.slide-03,.slide-04,.slide-05,.slide-06 {
+  clear:both;
+  width:100%;
+  height:0px;
+  overflow: hidden;
+  list-style-type: none;
+  transition: height .4s ease;
+}
+
+#touch,#touch-02,#touch-03,#touch-04,#touch-05,#touch-06 {position: absolute; opacity: 0; height: 0px; }
+
+.img-00{
+  width: 20px;
+  transform: rotateX(0deg) rotateY(180deg) rotateZ(0deg);
+
+}
+
+.flex{
+  display: flex;justify-content: space-between;align-items: center;
+}
+
+.cstm_ul{
+  font-size: 12px;
+}
+
+#touch:checked + .slide{height: 1000px;} 
+#touch-02:checked + .slide-02 {height: 150px;} 
+#touch-03:checked + .slide-03 {height: 200px;}
+#touch-04:checked + .slide-04 {height: 49px;} 
+#touch-05:checked + .slide-05 {height: 49px;} 
+#touch-06:checked + .slide-06 {height: 49px;} 
+
+
+  </style>
+
+  <label for="touch"><span class="span">التصنيفات</span></label>               
+  <input type="checkbox" id="touch"> 
+
+  <ul class="slide" style="color:white">
+    <li>
+        <label for="touch-02" class="flex">
+        <img src="male.png" class="img-00"/>
+        <div style="width: 185px;">
+        <span class="span-02">قسم الرجال</span>
+        </div>
+        </label>
+        <input type="checkbox" id="touch-02"> 
+
+        <ul class="slide-02 cstm_ul" style="color:white">
+          <li class="flex">
+          <img src="rucksack.png" class="img-00"/>
+          <div style="width: 147px;">
+          <a href="#">حقيبة الظهر الرجالية</a>
+          </div>
+
+          </li>
+
+          <li class="flex">
+          <img src="muscle.png" class="img-00"/>
+          <div style="width: 148px;">
+            <a href="#">الرياضة واللياقة البدنية</a>
+          </div>
+          </li>
+
+          <li class="flex">
+          <img src="hat.png" class="img-00"/>
+
+          <div style="width: 147px;">
+          <a href="#">قبعات رجالية</a>
+          <div>
+          
+          </li>
+
+        </ul>
+    </li>
+
+    <li>
+      <label for="touch-03" style="display: flex;justify-content: space-between;align-items: center;">
+      <img src="female.png" class="img-00"/>
+      <div style="width:185px">
+      <span class="span-03">قسم النساء</span>
+      </div>
+      
+      </label>
+      <input type="checkbox" id="touch-03"> 
+
+      <ul class="slide-03 cstm_ul" style="color:white">
+        <li class="flex">
+        <img src="rucksack.png" class="img-00"/>
+
+        <div style="width: 145px;">
+          <a href="#">حقيبة الظهر  النسائية</a>
+        </div>
+        </li>
+
+        <li class="flex">
+        <img src="cosmetic.png" class="img-00"/>
+        <div style="width: 145px;">
+        <a href="#">الصحة و الجمال</a>
+        </div>
+        </li>
+
+        </li>
+
+        <li class="flex">
+        <img src="muscle.png" class="img-00"/>
+        <div style="width: 148px;">
+          <a href="#">الرياضة واللياقة البدنية</a>
+        </div>
+        </li>
+
+
+        <li class="flex">
+        <img src="hat-fm.png" class="img-00"/>
+
+        <div style="width: 148px;">
+        <a href="#">قبعات نسائية </a>
+        </div>
+        </li>
+      </ul>
+    </li>
+
+    <li>
+    <label for="touch-04" class="flex">
+
+    <img src="baby.png" class="img-00"/>
+    <div style="width: 185px;">
+    <span class="span-04">قسم الأطفال</span>
+    </div>
+    
+    </label>
+      <input type="checkbox" id="touch-04"> 
+
+      <ul class="slide-04 cstm_ul" style="color:white">
+        
+        <li>
+            <a href="#">منتجات العناية بالأطفال</a>
+            
+        </li>
+
+      </ul>
+      
+    </li>
+    
+    <li>
+    <label for="touch-05" class="flex">
+    <img src="home.png" class="img-00"/>
+    
+    <div style="width: 185px;">
+    <span class="span-05">قسم المستلزمات المنزلية</span>
+    </div>
+
+    </label>
+      <input type="checkbox" id="touch-05"> 
+
+      <ul class="slide-05 cstm_ul" style="color:white">
+        <li>
+        
+        <a href="#">معدات و مستلزمات المنزل</a>
+        
+
+        </li>
+
+      </ul>
+      
+    </li>
+
+
+    <li>
+    <label for="touch-06" class="flex">
+    <img src="electro.png" class="img-00"/>
+    
+    <div style="width: 185px;">
+    <span class="span-06">قسم الإلكترونيات</span>
+    </div>
+
+    </label>
+      <input type="checkbox" id="touch-06"> 
+
+      <ul class="slide-06 cstm_ul" style="color:white">
+        <li>
+        
+        <a href="#">الإلكترونيات و اكسسوارات</a>
+        
+
+        </li>
+
+      </ul>
+      
+    </li>
+
+
+
+
+
+    </ul>
+
+ 
+
+  `);
+  
+  
+  originalLi.parentNode.replaceChild(newLi, originalLi);
+
+
+  
+
+});
+
+
+
